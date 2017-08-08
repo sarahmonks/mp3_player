44a697b80299bc16e1b443544738f8dcb04275c8
Sarah Monks <monks.sarah@gmail.com>
2017-08-08 21:27:44 +0100

    Tidied the code adding descriptive comments

--- src/components/SoundsList/SoundsList.js
+++ src/components/SoundsList/SoundsList.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import './sounds-list.css';
 const SoundsList = props => {
-	//This is a stateless component which contains a list of sounds.
-	//You can navigate to a particular sound in the list by clicking it.
+	//This is a stateless component used in the AppContainer container
+    //This component contains the HTML for the list of sounds that can be played with the audio player.
+    //You can navigate to a particular sound in the list by clicking it.
+
+    //To this component, we pass in the following states as properties (These states are defined in AppContainer):
+    //currentSoundIndex (integer)
+
+ 	//And we pass in the following methods as properties (defined in AppContainer):
+	//selectSound (parameters: index (integer))
+
+    //We also pass in the sounds array of JSON data which is passed in as a property to the AppContainer in src/index.js
+    //sounds (array)
+
 	 return (<div className="scrollable-container scrollable">
 			<div id="list-of-sounds-container">
 				<ul id="list-of-sounds">
--- src/components/TimerBar/TimerBar.js
+++ src/components/TimerBar/TimerBar.js
@@ -2,18 +2,19 @@ import React from 'react';
 import './timer-bar.css';
 
 const TimerBar = props => {
-	//This is a stateless component for the timer bar of the audio player. 
-	//
+	//This is a stateless component used in the AppContainer container
+	//This component contains the HTML for the timer bar area of the audio player. 
 
 	//To this component, we pass in the following states as properties (These states are defined in AppContainer):
     //currentSoundIndex (integer)
-    //sounds (array)
     //currentWidthOfTimerBar (float)
     //currentTime (string)
 
     //And we pass in the following method as a property (defined in AppContainer):
     //secondsToMins (parameter: seconds (integer))
 
+    //We also pass in the sounds array of JSON data which is passed in as a property to the AppContainer in src/index.js
+    //sounds (array)
 
 	//firstly we get the duration of the current sound in the player so we can display it in the controls area.
 	var duration;
