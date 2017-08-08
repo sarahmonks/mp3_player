dbf62b55ff0ebb1137a82c32623257fbd80ab5d5
Sarah Monks <monks.sarah@gmail.com>
2017-08-08 21:20:55 +0100

    Tidied the code adding descriptive comments

--- src/components/SidePanel/SidePanel.js
+++ src/components/SidePanel/SidePanel.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import './side-panel.css';
 const SidePanel = props => {
-	
+	//This is a stateless component used in the AppContainer container
+    //This component contains the HTML for the side panel area of the application
+    //We don't need to pass in any properties to this component
+
 	 return (<div id="side-panel-area" className="scrollable">		
 			<div className="side-panel-container">
 				<div className="side-panel-header"></div>
