57645737d91dadf11d03a3d33dce31bbf2b13185
Sarah Monks <monks.sarah@gmail.com>
2017-08-08 20:27:15 +0100

    Added in descriptive comments and changed var to let for variables

--- src/components/Display/Display.js
+++ src/components/Display/Display.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import './display.css';
 const Display = props => {
-	//This component displays the title and artist of the sound which is currently playing.
-	var title;
-	var artist;
+	//This is a stateless component used in the AppContainer container
+    //This component contains the HTML to display the title and artist of the sound which is currently playing.
+    
+    //To this component, we pass in the following states as properties (These states are defined in AppContainer):
+    //currentSoundIndex (integer)
+
+    //We also pass in the sounds array of JSON data which is passed in as a property to the AppContainer in src/index.js
+    //sounds (array)
+
+    let title = null;
+    let artist = null;
+
 	if(props.sounds[props.currentSoundIndex]){
 		title = props.sounds[props.currentSoundIndex].title;
 		artist = props.sounds[props.currentSoundIndex].artist;
@@ -13,14 +22,14 @@ const Display = props => {
 
 	}
 	 return (<div id="display-area">
-			<span className="sound-title">
-				{title}
-			</span>
-			<span className="sound-artist">
-				{artist}
-			</span>	
+				<span className="sound-title">
+					{title}
+				</span>
+				<span className="sound-artist">
+					{artist}
+				</span>	
 		
-		</div>          	
+			</div>          	
     	);
 }
 
